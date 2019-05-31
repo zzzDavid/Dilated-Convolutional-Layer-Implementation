2a145b7ede0cd6740ed8d4343501b679be228b16
zzzDavid <33577135+zzzDavid@users.noreply.github.com>
2019-06-01 02:15:54 +0800

    fix of dilated-cpu-forward

--- src/im2col_dilated.c
+++ src/im2col_dilated.c
@@ -16,14 +16,14 @@ void im2col_dilated_cpu(float* data_im,
     int width_col = (width + 2*pad - dilate_ksize) / stride + 1;
     int channels_col = channels * ksize * ksize;
     for (c = 0; c < channels_col; ++c) {
-        int w_offset = c % ksize;
-        int h_offset = (c / ksize) % ksize;
+        int w_offset = c % ksize + 1;
+        int h_offset = (c / ksize) % ksize + 1;
         int c_im = c / ksize / ksize;
-        for (h = 1; h <= height_col; ++h) {
-            for (w = 1; w <= width_col; ++w) {
+        for (h = 0; h < height_col; ++h) {
+            for (w = 0; w < width_col; ++w) {
                 int im_row = h_offset * dilate_rate + h * stride;
                 int im_col = w_offset * dilate_rate + w * stride;
-                int col_index = (c * height_col + h) * width_col + w - (width_col+1);
+                int col_index = (c * height_col + h) * width_col + w;
                 data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
                         im_row, im_col, c_im, pad);
             }
