27c8d3eb1835947521114113bb7780c36b889c61
Niansong Zhang <2033203466@qq.com>
2019-06-01 10:07:08 +0800

    cpu forward bug fix

--- src/im2col_dilated.c
+++ src/im2col_dilated.c
@@ -21,8 +21,8 @@ void im2col_dilated_cpu(float* data_im,
         int c_im = c / ksize / ksize;
         for (h = 0; h < height_col; ++h) {
             for (w = 0; w < width_col; ++w) {
-                int im_row = h_offset * dilate_rate + h * stride;
-                int im_col = w_offset * dilate_rate + w * stride;
+                int im_row = h_offset * dilate_rate + h * stride - 1;
+                int im_col = w_offset * dilate_rate + w * stride - 1;
                 int col_index = (c * height_col + h) * width_col + w;
                 data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
                         im_row, im_col, c_im, pad);
