5b6327322eacadd826d3d4cecb565170247b5102
zzzDavid <xiusj@cn2.gpyhpc.sysu>
2019-06-01 10:16:13 +0800

    clean up

--- src/im2col_dilated.c
+++ src/im2col_dilated.c
@@ -9,7 +9,7 @@ void im2col_dilated_cpu(float* data_im,
      int channels,  int height,  int width,
      int ksize,  int stride, int pad, float* data_col, int dilate_rate) 
 {
-    printf("Entering im2col_dilated_cpu\n");
+    //printf("Entering im2col_dilated_cpu\n");
     int c,h,w;
     int dilate_ksize = (dilate_rate - 1) * (ksize + 1) + ksize;
     int height_col = (height + 2*pad - dilate_ksize) / stride + 1;
@@ -26,7 +26,7 @@ void im2col_dilated_cpu(float* data_im,
                 int col_index = (c * height_col + h) * width_col + w;
                 data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
                         im_row, im_col, c_im, pad);
-		printf("im_row = %d, im_col = %d, pixel = %f\n", im_row, im_col, data_col[col_index]);
+		//printf("im_row = %d, im_col = %d, pixel = %f\n", im_row, im_col, data_col[col_index]);
 
             }
         }
